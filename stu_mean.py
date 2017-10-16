437d4d2fb035f581d26052d97677ce785c61a7a5
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-10-16 19:57:17 -0400

    put code into a try-except block

--- stu_mean.py
+++ stu_mean.py
@@ -14,49 +14,54 @@ import sqlite3   #enable control of an sqlite database
    3. The 'display' step shows the user the IDs, names, and averages of each 
    student.'''
 
-f = "discobandit.db"
+try:
+    f = "discobandit.db"
 
-db = sqlite3.connect(f) #open if f exists, otherwise create
-c = db.cursor()    #facilitate db ops
+    db = sqlite3.connect(f) #open if f exists, otherwise create
+    c = db.cursor()    #facilitate db ops
 
-d = {} #dictionary for storing student data
+    d = {} #dictionary for storing student data
 
-#obtain all marks matched with each ID ('construct' step)
-values = c.execute("SELECT peeps.id,name,mark FROM peeps,courses WHERE peeps.id = courses.id")
-for row in values:
-    ID = row[0]
-    name = str(row[1])
-    mark = row[2]
+    #obtain all marks matched with each ID ('construct' step)
+    values = c.execute("SELECT peeps.id,name,mark FROM peeps,courses WHERE peeps.id = courses.id")
+    for row in values:
+        ID = row[0]
+        name = str(row[1])
+        mark = row[2]
 
-    #if ID is not in the dictionary yet, create a key for it
-    if ID not in d:
-        d[ID] = [name,mark]
-    #otherwise, add to the key with the proper ID
-    else:
-        d[ID].append(mark)
+        #if ID is not in the dictionary yet, create a key for it
+        if ID not in d:
+            d[ID] = [name,mark]
+        #otherwise, add to the key with the proper ID
+        else:
+            d[ID].append(mark)
 
-    #print ID,":",d[ID]
+        #print ID,":",d[ID]
 
-#print "\n"
+    #print "\n"
 
-#compute averages for each ID and replace marks with said average ('modify' step)
-for ID in d:
-    marks = d[ID][1:len(d[ID])] #list of all marks for each ID
-    avg = sum(marks) / float(len(marks)) #average of marks for each ID
+    #compute averages for each ID and replace marks with said average ('modify' step)
+    for ID in d:
+        marks = d[ID][1:len(d[ID])] #list of all marks for each ID
+        avg = sum(marks) / float(len(marks)) #average of marks for each ID
 
-    #remove all marks to be replaced by their average
-    while len(d[ID]) > 1:
-        d[ID].pop()
-    d[ID].append(avg) #insert average to the value
+        #remove all marks to be replaced by their average
+        while len(d[ID]) > 1:
+            d[ID].pop()
+        d[ID].append(avg) #insert average to the value
 
-    #print ID,":",d[ID]
+        #print ID,":",d[ID]
 
-#display IDs, names, and averages ('display' step)
-print "--------------------------------------"
-for ID in sorted(d.iterkeys()):
-    name = d[ID][0]
-    avg = d[ID][1]
-    print "ID:", ID
-    print "Name:",name
-    print "Average:",avg
+    #display IDs, names, and averages ('display' step)
     print "--------------------------------------"
+    for ID in sorted(d.iterkeys()):
+        name = d[ID][0]
+        avg = d[ID][1]
+        print "ID:", ID
+        print "Name:",name
+        print "Average:",avg
+        print "--------------------------------------"
+except:
+    print "E R R O R -- check if:"
+    print "(1)the database of the appropriate name is in the same directory"
+    print "(2)the database contains the proper tables (peeps and courses) with the proper fields"
