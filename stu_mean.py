45572b1f16f1d7908d9c5e859a1c8b6ca158e41c
queeniexiang <wxiang1@stuy.edu>
2017-10-17 14:27:08 -0400

    Separated functions

--- stu_mean.py
+++ stu_mean.py
@@ -22,24 +22,38 @@ try:
 
     d = {} #dictionary for storing student data
 
-    #obtain all marks matched with each ID ('construct' step)
-    values = c.execute("SELECT peeps.id,name,mark FROM peeps,courses WHERE peeps.id = courses.id")
-    for row in values:
-        ID = row[0]
-        name = str(row[1])
-        mark = row[2]
-
-        #if ID is not in the dictionary yet, create a key for it
-        if ID not in d:
-            d[ID] = [name,mark]
-        #otherwise, add to the key with the proper ID
-        else:
-            d[ID].append(mark)
-
-        #print ID,":",d[ID]
+except:
+    print "E R R O R -- check if:"
+    print "(1)the database of the appropriate name is in the same directory"
     
-    #print "\n"
+    
 
+def match(): 
+    #obtain all marks matched with each ID ('construct' step)
+    try:
+        values = c.execute("SELECT peeps.id,name,mark FROM peeps,courses WHERE peeps.id = courses.id")
+        for row in values:
+            ID = row[0]
+            name = str(row[1])
+            mark = row[2]
+
+            #if ID is not in the dictionary yet, create a key for it
+            if ID not in d:
+                d[ID] = [name,mark]
+                #otherwise, add to the key with the proper ID
+            else:
+                d[ID].append(mark)
+                
+        #print ID,":",d[ID]
+        #print "\n"
+
+    except:
+         print "E R R O R -- check if the database contains the proper tables (peeps and courses) with the proper fields"
+        
+
+
+
+def compute_average(): 
     #compute averages for each ID and replace marks with said average ('modify' step)
     for ID in d:
         marks = d[ID][1:len(d[ID])] #list of all marks for each ID
@@ -52,15 +66,28 @@ try:
 
         #print ID,":",d[ID]
 
-    c.execute("DROP TABLE IF EXISTS peeps_avg")
-    c.execute("CREATE TABLE peeps_avg (id INTEGER, avg REAL)")
-    for ID in d:
-        avg = d[ID][1]
-        c.execute("INSERT INTO peeps_avg VALUES(" + str(ID) + "," + str(avg) + ")")
-        print ID,avg
+        
+def insert():
+
+    try: 
+        #To avoid adding duplicate values 
+        c.execute("DROP TABLE IF EXISTS peeps_avg")
+
+        #Create a new table with columns id and avg 
+        c.execute("CREATE TABLE peeps_avg (id INTEGER, avg REAL)")
+
+        #Add values to the table in their appropriate columns from the dictionary
+        for ID in d:
+            avg = d[ID][1]
+            c.execute("INSERT INTO peeps_avg VALUES(" + str(ID) + "," + str(avg) + ")")
+            #print ID,avg
+    except:
+        print "E R R O R -- check if the table was created and called properly"
+        
+        
 
 
-    
+def print_dict(): 
     #display IDs, names, and averages ('display' step)
     print "--------------------------------------"
     for ID in sorted(d.iterkeys()):
@@ -71,12 +98,12 @@ try:
         print "Average:",avg
         print "--------------------------------------"
 
-    db.commit()
-    db.close()
-except:
-    print "E R R O R -- check if:"
-    print "(1)the database of the appropriate name is in the same directory"
-    print "(2)the database contains the proper tables (peeps and courses) with the proper fields"
+
+
+
+db.commit()
+db.close()
+
 
 
 
